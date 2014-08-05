eadf91b936ccfe114f866a6e805fe8dfe9dec73c
shalk <xshalk@163.com>
2014-08-05 15:57:46 +0800

    delete bak

--- utility/bak/oracleadd.sh
+++
@@ -1,7 +0,0 @@
-chmod 777 /database//p*.zip
-su - oracle  -c ' [ -d /home/oracle/database ] ||  unzip /database/p*1of7.zip -d /home/oracle && unzip /database/p*2of7.zip -d /home/oracle '
-mkdir  /root/.xauth/
-echo oracle >  /root/.xauth/export
-echo ================Excute this ============================
-echo "cd database ; ./runInstaller -ignoreSysPrereqs "
-su - oracle
